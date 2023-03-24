eb3a1a7f229a63195b01291d480c5fa3d51f8d93
Mir Ashiful Haque <mirashifulhaque@gmail.com>
2023-03-24 03:31:00 +0000

    Added Comments
    Added Comments in 2 files for better readability

--- flask-server/DataCleaning.py
+++ flask-server/DataCleaning.py
@@ -1,69 +1,58 @@
-import pandas as pd
-import re
-import string
+# Import required libraries
+import pandas as pd  # For data manipulation and analysis
+import re  # For regular expressions
+import string  # For string operations
 
-
-df_fake = pd.read_csv("D:/Python Projects/Fake-News-Detection-Application\DataSets\Data Set\Fake.csv") 
+# Read in the fake and true news datasets as separate dataframes
+df_fake = pd.read_csv("D:/Python Projects/Fake-News-Detection-Application\DataSets\Data Set\Fake.csv")
 df_true = pd.read_csv("D:/Python Projects/Fake-News-Detection-Application\DataSets\Data Set\True.csv")
 
-
+# Print the first five rows of both dataframes
 df_fake.head()
-
-
 df_true.head()
 
-
+# Add a new column to the fake dataframe called "Status" with the value "Fake"
 df_fake["Status"]="Fake"
-df_fake.head()
-
-
+# Add a new column to the true dataframe called "Status" with the value "True"
 df_true["Status"]="True"
+
+# Print the first five rows of both dataframes with the new columns included
+df_fake.head()
 df_true.head()
 
-
+# Concatenate the two dataframes into a single dataframe called "df_all"
 frames=[df_true,df_fake]
-
 df_all=pd.concat(frames)
+
+# Print the first five rows of the combined dataframe
 df_all.head()
 
-
+# Save the dataframe to a CSV file called "All.csv" in the specified directory
 df_all.to_csv("D:/Python Projects/Fake-News-Detection-Application\DataSets\Data Set\All.csv", encoding="UTF-8", index=False)
 
-
-
+# Define a function to preprocess text data
 def wordopt(text):
-    text = text.lower()
-    text = re.sub('\[.*?\]', '', text)
-    text = re.sub("\\W"," ",text) 
-    text = re.sub('https?://\S+|www\.\S+', '', text)
-    text = re.sub('<.*?>+', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text)    
+    text = text.lower()  # Convert text to lowercase
+    text = re.sub('\[.*?\]', '', text)  # Remove text within square brackets
+    text = re.sub("\\W"," ",text)  # Replace non-word characters with space
+    text = re.sub('https?://\S+|www\.\S+', '', text)  # Remove URLs
+    text = re.sub('<.*?>+', '', text)  # Remove HTML tags
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)  # Remove punctuation
+    text = re.sub('\n', '', text)  # Remove newline characters
+    text = re.sub('\w*\d\w*', '', text)  # Remove words containing digits
     return text
 
-
+# Apply the preprocessing function to the "text" column of the combined dataframe
 df_all["text"] = df_all["text"].apply(wordopt)
-df_all.head()
 
-
+# Create a new dataframe called "df" with only the "text" and "Status" columns from the combined dataframe
 df=df_all[["text","Status"]].copy()
-df.head()
 
+# Replace the values in the "Status" column with integers (1 for "True" and 0 for "Fake")
+df["Status"]= df["Status"].replace({"True" : 1, "Fake" : 0})
 
-df["Status"]= df["Status"].replace({"True" : 1})
-df["Status"]= df["Status"].replace({"Fake" : 0})
-df.head()
-
-
-
+# Rename the "Status" column to "status"
 df=df.rename(columns={"Status":"status"})
-df.head()
 
-
+# Save the cleaned dataframe to a CSV file called "Clean.csv" in the specified directory
 df.to_csv("D:/Python Projects/Fake-News-Detection-Application\DataSets\Data Set\Clean.csv", encoding="UTF-8", index=False)
-
-
-
-
-
--- flask-server/cleaningArticle.py
+++ flask-server/cleaningArticle.py
@@ -1,25 +1,36 @@
+# import necessary modules
 import csv
 import re
 import string
 
+# function to clean text
 def wordopt(text):
-    text = re.sub('\[.*?\]', '', text)
-    text = re.sub("\\W"," ",text) 
-    text = re.sub('https?://\S+|www\.\S+', '', text)
-    text = re.sub('<.*?>+', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
-    text = re.sub('\n', '', text)
-    text = re.sub('\w*\d\w*', '', text)    
+    text = re.sub('\[.*?\]', '', text) # remove text within square brackets
+    text = re.sub("\\W"," ",text) # replace non-alphanumeric characters with whitespace
+    text = re.sub('https?://\S+|www\.\S+', '', text) # remove URLs
+    text = re.sub('<.*?>+', '', text) # remove HTML tags
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text) # remove punctuation
+    text = re.sub('\n', '', text) # remove newlines
+    text = re.sub('\w*\d\w*', '', text) # remove words containing digits
     return text
 
+# open output file for writing cleaned data
 with open('./articles/articleClean.csv', 'w', newline='', encoding='utf-8') as output_file:
+    # create csv writer object
     writer = csv.writer(output_file)
+    # write header row
     writer.writerow(['title', 'text'])
 
+    # open input file for reading
     with open('./articles/article.csv', 'r', newline='', encoding='utf-8') as input_file:
+        # create csv reader object
         reader = csv.reader(input_file)
         next(reader) # skip header row
+        # iterate over rows in input file
         for row in reader:
+            # extract title and text from current row
             title, text = row
+            # clean text using wordopt function
             text = wordopt(text)
+            # write cleaned title and text to output file
             writer.writerow([title, text])
